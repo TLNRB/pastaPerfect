2ef6fc5ee2b1f2263ff8bc10025559a7cc106f1e
TLNRB <nortol01@easv365.dk>
2023-03-19 11:13:56 +0100

    Nav styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const nav = document.querySelector("#nav");
 const darkMode = document.querySelector("#dark-mode");
+const lightMode = document.querySelector("#light-mode");
 const hamburger = document.querySelector("#hamburger");
 const closeButton = document.querySelector("#close-button");
+
+darkMode.addEventListener("click", () => {
+  lightMode.classList.add("button-is-active");
+  darkMode.classList.add("button-isnot-active");
+});
+
+lightMode.addEventListener("click", () => {
+  lightMode.classList.remove("button-is-active");
+  darkMode.classList.remove("button-isnot-active");
+});
 
 hamburger.addEventListener("click", () => {
   nav.classList.add("is-active");
