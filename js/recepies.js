128fda2815b993c028862cc4f62c766bd7f7cc69
TLNRB <nortol01@easv365.dk>
2023-03-21 11:13:04 +0100

    Images added and recepies array started

---
+++ js/recepies.js
@@ -0,0 +1,28 @@
+const recepies = [
+  {
+    id: 1,
+    title: "Tomato",
+    fullName: "Spaghetti Tomato",
+    smallDescription: "A symbolic dish of Italian cuisine, with the typical ingredients of the Bel Paese.",
+    description: "A symbolic dish of Italian cuisine, with the typical ingredients of the Bel Paese: spaghetti, tomato and basil. Spaghetti with tomato sauce is an institution, a simple but far from trivial recipe.",
+    pastaType: "./images/types/spaghetti.svg",
+    pastaTypeName: "Spaghetti",
+    levelOfSkill: "Easy",
+    time: "1 hr",
+    ingridients: [
+      "375 g spaghetti",
+      "30 ml olive oil",
+      "800 g Peeled tomatoes",
+      "4 cloves garlic, minced",
+      "Salt and pepper",
+      "Basil to taste"
+    ],
+    preparation: [
+        "Start by preparing the sauce. Pour the extra virgin olive oil in a pan with the peeled and halved garlic clove, so you can remove the core and make the scent more delicate. Cook for 2 minutes over high heat, add the peeled tomatoes and salt.",
+        "Cover with a lid and cook for at least 1 hour over very low heat: the sauce should simmer gently. Mix it from time to time. After this time, remove the garlic and pass the tomatoes through a sieve to obtain a smooth and homogeneous puree.",
+        "Pour the sauce back into the pan, turn the heat to very low and add the basil leaves. After a few minutes, turn off the sauce and keep it warm. All you have to do now is cook the pasta in plenty of boiling salted water.",
+        "Drain the spaghetti al dente directly into the sauce and stir for a few moments over high heat to mix everything together. Your spaghetti with tomato sauce is ready, all you have to do is serve it, garnished with fresh basil to taste."
+    ],
+    picture: "./images/dishes/tomato.svg"
+  },
+]
