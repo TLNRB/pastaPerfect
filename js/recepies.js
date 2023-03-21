a1b944a0edc38c2ac613fef35bef207474d1b0a7
TLNRB <nortol01@easv365.dk>
2023-03-21 18:19:50 +0100

    Recepies added fully

--- js/recepies.js
+++ js/recepies.js
@@ -11,9 +11,9 @@ const recepies = [
     levelOfSkill: "Easy",
     time: "1 hr",
     ingridients: [
-      "375 g spaghetti",
+      "375g spaghetti",
       "30 ml olive oil",
-      "800 g Peeled tomatoes",
+      "800g Peeled tomatoes",
       "4 cloves garlic, minced",
       "Salt and pepper",
       "Basil to taste"
@@ -22,9 +22,11 @@ const recepies = [
         "Start by preparing the sauce. Pour the extra virgin olive oil in a pan with the peeled and halved garlic clove, so you can remove the core and make the scent more delicate. Cook for 2 minutes over high heat, add the peeled tomatoes and salt.",
         "Cover with a lid and cook for at least 1 hour over very low heat: the sauce should simmer gently. Mix it from time to time. After this time, remove the garlic and pass the tomatoes through a sieve to obtain a smooth and homogeneous puree.",
         "Pour the sauce back into the pan, turn the heat to very low and add the basil leaves. After a few minutes, turn off the sauce and keep it warm. All you have to do now is cook the pasta in plenty of boiling salted water.",
-        "Drain the spaghetti al dente directly into the sauce and stir for a few moments over high heat to mix everything together. Your spaghetti with tomato sauce is ready, all you have to do is serve it, garnished with fresh basil to taste."
+        "Drain the spaghetti al dente directly into the sauce and stir for a few moments over high heat to mix everything together. Your spaghetti with tomato sauce is ready, all you have to do is serve it, garnished with fresh basil to taste.",
+        "Buon Appetito!"
     ],
-    picture: "./images/dishes/tomato.svg"
+    picture: "./images/dishes/tomato.svg",
+    serving: 4
   },
   //==========Pasta Alla Boscaiola==========
   {
@@ -38,12 +40,12 @@ const recepies = [
     levelOfSkill: "Easy",
     time: "30 min",
     ingridients: [
-      "400 g penne pasta",
+      "400g penne pasta",
       "1 shallot",
-      "180 g crimini mushrooms",
+      "180g crimini mushrooms",
       "2 small Italian sausages",
-      "150 ml heavy cream",
-      "25 g Parmigiano Reggiano finely grated",
+      "150ml heavy cream",
+      "25g Parmigiano Reggiano finely grated",
       "1 small bunch fresh parsley",
       "1-2 tablespoon olive oil",
       "Salt and pepper for seasoning"
@@ -53,9 +55,11 @@ const recepies = [
         "Meanwhile, heat the olive oil in a large skillet, once hot add the finely chopped shallot and saute for 1-2 minutes until softened.",
         "Add the sliced mushrooms and fry until softened (3-4 minutes). Next, add the Italian sausages removed from their casing and cook until browned.",
         "Add the cream, Parmigiano Reggiano and a good pinch of salt and pepper. Stir then simmer gently for a few minutes until thickened slightly.",
-        "Add the cooked pasta with a splash of reserved pasta water to loosen the sauce if needed. Add the parsley, toss to combine then serve with more pepper and parmesan."
+        "Add the cooked pasta with a splash of reserved pasta water to loosen the sauce if needed. Add the parsley, toss to combine then serve with more pepper and parmesan.",
+        "Buon Appetito!"
     ],
-    picture: "./images/dishes/PASTA ALLA BOSCAIOLA.svg"
+    picture: "./images/dishes/PASTA ALLA BOSCAIOLA.svg",
+    serving: 4
   },
   //==========Ravioli==========
   {
@@ -69,13 +73,13 @@ const recepies = [
     levelOfSkill: "Average",
     time: "1 hr 15 min",
     ingridients: [
-      "250 g Flour",
+      "250g Flour",
       "2 Eggs",
       "1 Egg yolks",
-      "250 g Spinach",
-      "125 g Ricotta cheese",
-      "50 g Grana Padano DOP cheese",
-      "40 g Butter",
+      "250g Spinach",
+      "125g Ricotta cheese",
+      "50g Grana Padano DOP cheese",
+      "40g Butter",
       "5 leaves Sage",
       "Remilled durum wheat semolina to taste",
       "Nutmeg to taste",
@@ -93,40 +97,154 @@ const recepies = [
         "Lay the other sheet on top, making sure they match and stick at the edges, to avoid breaking the ravioli while cooking, let the air out between one raviolo and the next, exerting light pressure with your fingers from the center of the filled raviolo outwards. Then take a notched pastry wheel and make 24 ravioli of 4x4 cm.",
         "As you cut the ravioli, place them on a tray lightly floured with semolina and spaced apart so that they do not stick together. Now bring a pot with salted water to a boil and add the ravioli as soon as it boils. While the ricotta and spinach ravioli are cooking, melt the butter in a pan over very low heat.",
         "Add previously-washed, roughly chopped sage leaves and mix the ingredients. At this point the ravioli that come up to the surface of the water are ready.",
-        "Transfer them with a slotted spoon directly into the pan with the sauce 25 to flavor them for a few moments. If necessary, add a ladle of hot water so that the sauce does not dry out too much and finish cooking."
+        "Transfer them with a slotted spoon directly into the pan with the sauce 25 to flavor them for a few moments. If necessary, add a ladle of hot water so that the sauce does not dry out too much and finish cooking.",
+        "Buon Appetito!"
     ],
-    picture: "./images/dishes/ravioli.svg"
+    picture: "./images/dishes/ravioli.svg",
+    serving: 4
   },
-  //==========Pasta Alla Boscaiola==========
+  //==========Lasagna==========
   {
     id: 4,
     title: "Lasagne",
-    fullName: "Lasagne alla Bolognese",
-    smallDescription: "Layers of green egg pasta, dressed with the classic traditional ragù, béchamel and grated cheese.",
-    description: "This rich and tasty dish originates from the Emilia region, specifically, from the city of Bologna. Layers of green egg pasta, dressed with the classic traditional ragù, béchamel and grated cheese. ",
+    fullName: "Lasagne al Forno",
+    smallDescription: "Layers of pasta, dressed with the classic traditional ragu, bechamel and grated cheese.",
+    description: "This rich and tasty Italian dish made with homemade beef ragu, bechamel sauce, silky pasta and topped with melted mozzarella cheese.",
     pastaType: "./images/types/lasagna.svg",
     pastaTypeName: "Lasagne",
-    levelOfSkill: "Average",
-    time: "3 hr",
+    levelOfSkill: "Hard",
+    time: "4 hr",
     ingridients: [
-      "400 g penne pasta",
-      "1 shallot",
-      "180 g crimini mushrooms",
-      "2 small Italian sausages",
-      "150 ml heavy cream",
-      "25 g Parmigiano Reggiano finely grated",
-      "1 small bunch fresh parsley",
-      "1-2 tablespoon olive oil",
+      "500g lasagna pasta sheets",
+      "250g mozzarella cheese",
+      "1 tablespoon olive oil",
+      "1 large carrot finely chopped",
+      "1 large white onion finely chopped",
+      "500g ground beef (mince)",
+      "500g ground pork (mince)",
+      "540g sieved tomatoes tomatoes (passata)",
+      "3 tablespoon tomato paste",
+      "250ml red wine",
+      "1.5l beef stock",
+      "2 bay leaves",
+      "70g butter",
+      "70g flour all-purpose",
+      "1l full fat milk",
+      "½ teaspoon nutmeg",
+      "70g parmesan freshly grated",
+      "2 tablespoons finely chopped fresh parsley",
       "Salt and pepper for seasoning"
     ],
     preparation: [
-        "Bring a large pot of water to a boil and salt it generously. Cook the pasta according to packet instructions until al dente.",
-        "Meanwhile, heat the olive oil in a large skillet, once hot add the finely chopped shallot and saute for 1-2 minutes until softened.",
-        "Add the sliced mushrooms and fry until softened (3-4 minutes). Next, add the Italian sausages removed from their casing and cook until browned.",
-        "Add the cream, Parmigiano Reggiano and a good pinch of salt and pepper. Stir then simmer gently for a few minutes until thickened slightly.",
-        "Add the cooked pasta with a splash of reserved pasta water to loosen the sauce if needed. Add the parsley, toss to combine then serve with more pepper and parmesan."
+        "Finely chop the carrot, celery and onion and sweat slowly with a little olive oil in a large, deep skillet or pan. Once you've sauteed the veggies, add the meat and brown whilst breaking it up with a wooden spoon.",
+        "Add the red wine and reduce by half. Once the wine has reduced, add the sieved tomatoes, tomato paste, bay leaf, 4 cups of the beef stock and a pinch of salt and pepper. Stir everything together and leave to simmer on a low heat for 2.5-3 hours uncovered. Add the rest of the beef stock half way through.",
+        "For the bechamel sauce add the butter to a saucepan and cook until melted and bubbling.",
+        "Add the flour to the melted butter and stir to form a paste. Let the flour cook for 1 minute. Slowly whisk half of the milk into the butter and flour constantly whisking to avoid any lumps. Once it has started to thicken add the rest of the milk, nutmeg, parmesan and a pinch of salt and pepper.",
+        "Continue to heat the sauce whilst stirring until thickened enough to coat the back of a wooden spoon. Take off the heat and set aside.",
+        "To assemble the lasagne, spoon a small amount of ragu on the very bottom of the baking dish. Top with and even layer of lasagna pasta sheets.",
+        "Add a few more spoons of ragu so the pasta is completely covered followed by white sauce. Repeat the layers of pasta, ragu and bechamel sauce finishing with a layer of pasta topped with bechamel sauce (you should have 4-5 layers of pasta). Cover the top layer of the lasagne with torn mozzarella and then bake in the oven until bubbling and golden.",
+        "Buon Appetito!"
     ],
-    picture: "./images/dishes/lasagna.svg"
+    picture: "./images/dishes/lasagna.svg",
+    serving: 12
   },
-  
+  //==========Carbonara==========
+  {
+    id: 5,
+    title: "Carbonara",
+    fullName: "Spaghetti alla Carbonara",
+    smallDescription: "Carbonara is one of the most famous Roman dishes, made with pasta, guanciale, eggs, Pecorino Romano, and black pepper.",
+    description: "This authentic recipe is made with just 5 simple ingredients; pasta, guanciale, eggs, Pecorino Romano, and black pepper. Our recipe will guide you through the do's and don'ts to make a Carbonara just as they do in Rome.",
+    pastaType: "./images/types/spaghetti.svg",
+    pastaTypeName: "Spaghetti",
+    levelOfSkill: "Easy",
+    time: "10 min",
+    ingridients: [
+      "425g spaghetti",
+      "3 large eggs",
+      "30g Pecorino Romano",
+      "150g guanciale cut into cube",
+      "2 teaspoon freshly ground black pepper plus more for serving"
+    ],
+    preparation: [
+        "Bring a large pot of salted water to a boil and add the spaghetti, cook until al dente (around 7 minutes).",
+        "Meanwhile, add the guanciale to a hot skillet and fry until brown and crispy then turn off the heat (do not drain the fat). While the guanciale is frying add the eggs, pecorino and pepper to a bowl, whisk until well combined.",
+        "Once the pasta is ready, remove it with kitchen tongs to the skillet with the guanciale and toss so it's well combined in the fat.",
+        "Next, add the egg mixture whilst tossing the pasta to ensure the eggs don't scramble. Once the sauce turns glossy and creamy it's ready, add extra pecorino and pepper to serve.",
+        "Buon Appetito!"
+    ],
+    picture: "./images/dishes/carbonara.svg",
+    serving: 4
+  },
+   //==========Spaghetti Bolognese==========
+   {
+    id: 6,
+    title: "Spaghetti Bolognese",
+    fullName: "Spaghetti Bolgnese",
+    smallDescription: "One of the best ever, the spaghetti bolognese is a super easy and a true Italian classic.",
+    description: "Spaghetti bolognese is just in the name, spaghetti tossed in a sauce made from tomatoes, minced beef, garlic, wine, herbs, and spices. It is a classic Italian dish.",
+    pastaType: "./images/types/spaghetti.svg",
+    pastaTypeName: "Spaghetti",
+    levelOfSkill: "Easy",
+    time: "3 hr",
+    ingridients: [
+      "500g Spaghetti",
+      "1 tbsp Olive oil",
+      "1 Onion diced",
+      "2 Carrots diced",
+      "2 Celery stalks diced",
+      "2-3 cloves Garlic",
+      "5-6 leaves Fresh Basil or 2 tsp dried basil",
+      "400g Canned Diced Tomatoes",
+      "500g Tomato Passata/Crushed Tomatoes",
+      "250ml water",
+      "2 tsp Dried Oregano",
+      "500g Ground Beef",
+      "Salt and pepper to taste"
+    ],
+    preparation: [
+        "In a large and deep frying pan heat 1 tbsp of olive oil, then add diced onions, carrots, celery, minced garlic and cook on low heat for 10 minutes without colouring the vegetables.",
+        "Then add ground beef and brown it until the beef is no longer pink inside while stirring the whole time and making sure to break up the beef. Pour in both types of canned tomatoes and water with a pinch of salt and pepper.",
+        "Sprinkle in some dried oregano and torn basil leaves (reserve a few small ones), bring everything to a boil. Then turn the heat down and let it simmer with a lid ajar for for 2.5-3 hours stirring it once in a while.",
+        "When your Bolognese is nearly done boil a large pot of water and cook spaghetti according to package directions.",
+        "Drain your spaghetti and add your pasta to the sauce, stir gently and take off the heat. Top with reserved basil leaves. Serve with freshly grated Parmesan cheese.",
+        "Buon Appetito!"
+    ],
+    picture: "./images/dishes/spaghetti.svg",
+    serving: 8
+  },
+  //==========Carbonara==========
+  {
+    id: 7,
+    title: "Alle Vongole",
+    fullName: "Alle Vongole",
+    smallDescription: "This classic seafood pasta recipe from Campania is simple to make and bursting with flavour.",
+    description: "Of all the wonderful seafood pasta recipes from Italy, pasta alle vongole is the top of the best seller list. It is flavoured with garlic, chilli, white wine and parsley.",
+    pastaType: "./images/types/spaghetti.svg",
+    pastaTypeName: "Spaghetti",
+    levelOfSkill: "Easy",
+    time: "40 min",
+    ingridients: [
+      "400g Spaghetti",
+      "800g fresh clams",
+      "3-5 garlic cloves, sliced",
+      "1/2 red chilli, deseeded and finely diced",
+      "4-5 tbsp extra virgin olive oil",
+      "1-2 handfuls fresh parsley finely chopped",
+      "80ml of white wine",
+      "Salt to taste"
+    ],
+    preparation: [
+        "Clean the clams by leaving to soak in heavily salted water (it should be salty enough to taste like sea water) for 20 minutes. The clams will naturally filter out any grit and sand. If they are particularly sandy, repeat the process with a fresh batch of salted water. After this process, leave to soak in unsalted cold water for 10 minutes to rinse off any excess salt they have absorbed.",
+        "Cook the spaghetti in salted boiling water until very al dente under the recommended cooking instructions.",
+        "Heat the olive oil in a large frying pan with a lid. Gently sauté the garlic slices and fresh chilli until soft but not coloured.",
+        "Add the clams to the pan followed by the white wine. Turn up the heat and cover with a lid.",
+        "Give the pan a shake every 20 seconds, the clams should be cooked after 2 minutes or so. Take the pan off the heat and discard any clams which have not opened, and any empty shells.",
+        "When ready, drain the pasta, reserving a cup of the cooking water. Toss the spaghetti through the white wine and clam sauce until well-coated, adding some of the reserved cooking water if needed.",
+        "Stir through the fresh parsley and leave for a minute or two for the pasta to absorb the flavours.",
+        "Buon Appetito!"
+    ],
+    picture: "./images/dishes/alleVongole.svg",
+    serving: 4
+  }
 ]
