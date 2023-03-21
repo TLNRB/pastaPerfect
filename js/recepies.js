bceee45469171bc5130c00554911c4f457b0beb8
TLNRB <nortol01@easv365.dk>
2023-03-21 14:12:32 +0100

    Recepie ravioli added

--- js/recepies.js
+++ js/recepies.js
@@ -28,29 +28,105 @@ const recepies = [
   },
   //==========Pasta Alla Boscaiola==========
   {
-    id: 1,
+    id: 2,
     title: "Pasta alla Boscaiola",
     fullName: "Pasta alla Boscaiola",
     smallDescription: "A a super easy pasta recipe made with a rich and creamy sausage and mushroom sauce.",
     description: "It literally translates as pasta woodland style which is a seasonal autumn dish so the most important ingredient is of course, mushrooms.",
-    pastaType: "./images/dishes/PASTA ALLA BOSCAIOLA.svg",
-    pastaTypeName: "Spaghetti",
+    pastaType: "./images/types/penne.svg",
+    pastaTypeName: "Penne",
     levelOfSkill: "Easy",
-    time: "1 hr",
+    time: "30 min",
     ingridients: [
-      "375 g spaghetti",
-      "30 ml olive oil",
-      "800 g Peeled tomatoes",
-      "4 cloves garlic, minced",
-      "Salt and pepper",
-      "Basil to taste"
+      "400 g penne pasta",
+      "1 shallot",
+      "180 g crimini mushrooms",
+      "2 small Italian sausages",
+      "150 ml heavy cream",
+      "25 g Parmigiano Reggiano finely grated",
+      "1 small bunch fresh parsley",
+      "1-2 tablespoon olive oil",
+      "Salt and pepper for seasoning"
     ],
     preparation: [
-        "Start by preparing the sauce. Pour the extra virgin olive oil in a pan with the peeled and halved garlic clove, so you can remove the core and make the scent more delicate. Cook for 2 minutes over high heat, add the peeled tomatoes and salt.",
-        "Cover with a lid and cook for at least 1 hour over very low heat: the sauce should simmer gently. Mix it from time to time. After this time, remove the garlic and pass the tomatoes through a sieve to obtain a smooth and homogeneous puree.",
-        "Pour the sauce back into the pan, turn the heat to very low and add the basil leaves. After a few minutes, turn off the sauce and keep it warm. All you have to do now is cook the pasta in plenty of boiling salted water.",
-        "Drain the spaghetti al dente directly into the sauce and stir for a few moments over high heat to mix everything together. Your spaghetti with tomato sauce is ready, all you have to do is serve it, garnished with fresh basil to taste."
+        "Bring a large pot of water to a boil and salt it generously. Cook the pasta according to packet instructions until al dente.",
+        "Meanwhile, heat the olive oil in a large skillet, once hot add the finely chopped shallot and saute for 1-2 minutes until softened.",
+        "Add the sliced mushrooms and fry until softened (3-4 minutes). Next, add the Italian sausages removed from their casing and cook until browned.",
+        "Add the cream, Parmigiano Reggiano and a good pinch of salt and pepper. Stir then simmer gently for a few minutes until thickened slightly.",
+        "Add the cooked pasta with a splash of reserved pasta water to loosen the sauce if needed. Add the parsley, toss to combine then serve with more pepper and parmesan."
     ],
-    picture: "./images/dishes/tomato.svg"
+    picture: "./images/dishes/PASTA ALLA BOSCAIOLA.svg"
   },
+  //==========Ravioli==========
+  {
+    id: 3,
+    title: "Ravioli",
+    fullName: "Ricotta and Spinach Ravioli",
+    smallDescription: "Ricotta and spinach ravioli with butter and sage.",
+    description: "The simplest and most delicious of sauces, butter and sage, encounters the stuffed pasta par excellence, ravioli: this is how a classic dish of Italian Sunday lunches takes shape.",
+    pastaType: "./images/types/ravioli.svg",
+    pastaTypeName: "Ravioli",
+    levelOfSkill: "Average",
+    time: "1 hr 15 min",
+    ingridients: [
+      "250 g Flour",
+      "2 Eggs",
+      "1 Egg yolks",
+      "250 g Spinach",
+      "125 g Ricotta cheese",
+      "50 g Grana Padano DOP cheese",
+      "40 g Butter",
+      "5 leaves Sage",
+      "Remilled durum wheat semolina to taste",
+      "Nutmeg to taste",
+      "Fine salt to taste",
+      "Black pepper to taste"
+    ],
+    preparation: [
+        "Start by making the pasta. Take a large bowl and pour in the flour (less than the recommended dose in order to add more as needed if the dough gets too sticky), then lightly beat the eggs and yolks, mix the ingredients well, kneading with your hands until you achieve the right consistency.",
+        "Transfer to a lightly floured work surface, continue to work the dough vigorously until it has a compact and elastic consistency. Shape the dough into a ball and cover it with plastic wrap so that it does not dry out. Let it rest for at least 30 minutes in a cool place, away from drafts to prevent it from drying out.",
+        "While the fresh pasta rests, dedicate yourself to the filling. Take a non-stick pan in which you place the well-rinsed spinach. Let them dry over low heat covered with a lid.",
+        "The spinach must cook until it is very soft, at this point you can drain and squeeze it well to make sure it releases all excess liquids and thus keep the dough from getting too wet. Finely chop the spinach. Take another bowl and pour in the ricotta and grated cheese.",
+        "Flavor with nutmeg and add the chopped spinach, mix the ingredients well and transfer the mixture to a disposable pastry bag (you can cut the tip quite wide) and set aside.",
+        "Take the fresh pasta dough and remove the plastic wrap. Lightly floured the dough with semolina and divide it into two equal pieces. Cover one of the two pieces with plastic wrap and take the other one. Roll the pasta till  you obtain a rectangular sheet with a thickness of about 2 mm and 10 cm wide. Then roll the second piece of dough the same way. Place them on a work surface lightly floured with semolina.",
+        "Then, using the pastry bag, place small dollops of filling on one of the two pasta sheets, about 3 cm away from each other. Spray or, if you prefer, brush the edges of the pastry with a little water, this will allow the two sheets to better stick together.",
+        "Lay the other sheet on top, making sure they match and stick at the edges, to avoid breaking the ravioli while cooking, let the air out between one raviolo and the next, exerting light pressure with your fingers from the center of the filled raviolo outwards. Then take a notched pastry wheel and make 24 ravioli of 4x4 cm.",
+        "As you cut the ravioli, place them on a tray lightly floured with semolina and spaced apart so that they do not stick together. Now bring a pot with salted water to a boil and add the ravioli as soon as it boils. While the ricotta and spinach ravioli are cooking, melt the butter in a pan over very low heat.",
+        "Add previously-washed, roughly chopped sage leaves and mix the ingredients. At this point the ravioli that come up to the surface of the water are ready.",
+        "Transfer them with a slotted spoon directly into the pan with the sauce 25 to flavor them for a few moments. If necessary, add a ladle of hot water so that the sauce does not dry out too much and finish cooking."
+    ],
+    picture: "./images/dishes/ravioli.svg"
+  },
+  //==========Pasta Alla Boscaiola==========
+  {
+    id: 4,
+    title: "Lasagne",
+    fullName: "Lasagne alla Bolognese",
+    smallDescription: "Layers of green egg pasta, dressed with the classic traditional ragù, béchamel and grated cheese.",
+    description: "This rich and tasty dish originates from the Emilia region, specifically, from the city of Bologna. Layers of green egg pasta, dressed with the classic traditional ragù, béchamel and grated cheese. ",
+    pastaType: "./images/types/lasagna.svg",
+    pastaTypeName: "Lasagne",
+    levelOfSkill: "Average",
+    time: "3 hr",
+    ingridients: [
+      "400 g penne pasta",
+      "1 shallot",
+      "180 g crimini mushrooms",
+      "2 small Italian sausages",
+      "150 ml heavy cream",
+      "25 g Parmigiano Reggiano finely grated",
+      "1 small bunch fresh parsley",
+      "1-2 tablespoon olive oil",
+      "Salt and pepper for seasoning"
+    ],
+    preparation: [
+        "Bring a large pot of water to a boil and salt it generously. Cook the pasta according to packet instructions until al dente.",
+        "Meanwhile, heat the olive oil in a large skillet, once hot add the finely chopped shallot and saute for 1-2 minutes until softened.",
+        "Add the sliced mushrooms and fry until softened (3-4 minutes). Next, add the Italian sausages removed from their casing and cook until browned.",
+        "Add the cream, Parmigiano Reggiano and a good pinch of salt and pepper. Stir then simmer gently for a few minutes until thickened slightly.",
+        "Add the cooked pasta with a splash of reserved pasta water to loosen the sauce if needed. Add the parsley, toss to combine then serve with more pepper and parmesan."
+    ],
+    picture: "./images/dishes/lasagna.svg"
+  },
+  
 ]
