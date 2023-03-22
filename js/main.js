af98fff19960544421ceaba3ed9f6f1ef2e0a01b
TLNRB <nortol01@easv365.dk>
2023-03-22 15:34:11 +0100

    JS dark/light mode button class bug fixed

--- js/main.js
+++ js/main.js
@@ -16,14 +16,12 @@ lightMode.addEventListener("click", () => {
 
 hamburger.addEventListener("click", () => {
   nav.classList.add("is-active");
-  darkMode.classList.add("button-isnot-active");
   hamburger.classList.add("button-isnot-active");
   closeButton.classList.add("button-is-active");
 });
 
 closeButton.addEventListener("click", () => {
   nav.classList.remove("is-active");
-  darkMode.classList.remove("button-isnot-active");
   hamburger.classList.remove("button-isnot-active");
   closeButton.classList.remove("button-is-active");
 });
