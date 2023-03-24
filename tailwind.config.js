c32899493cf584b63af1ce5486f2aed1badc44b2
TLNRB <nortol01@easv365.dk>
2023-03-24 14:27:35 +0100

    Headline added to recently section and updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      xxl: '1440px',
-      xxxl: '1600px'
+      xs: "360px",
+      sm: "525px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1440px",
+      xxxl: "1600px",
     },
     fontFamily: {
-      'sans-serif': 'Poppins',
-      'serif': 'Italiana',
-      'cursive': 'Italianno'
+      "sans-serif": "Poppins",
+      serif: "Italiana",
+      cursive: "Italianno",
     },
     extend: {
       colors: {
-        lightText: '#F4F4F4',
-        normalText: '#606060',
-        darkText: '#353535',
-        lightCardBG: '#F3F1EE',
-        normalCardBG: '#E9E5E0',
-        darkCardBG: '#ECE5DB',
-        lightRedText: '#D9042B',
-        darkRedBtn: '#8C0327',
-        darkBrown: '#A6907C',
-        darkestBrown: '#5A4B3D',
-      }
+        lightText: "#F4F4F4",
+        normalText: "#606060",
+        darkText: "#353535",
+        lightCardBG: "#F3F1EE",
+        normalCardBG: "#E9E5E0",
+        darkCardBG: "#ECE5DB",
+        lightRedText: "#D9042B",
+        darkRedBtn: "#8C0327",
+        darkBrown: "#A6907C",
+        darkestBrown: "#5A4B3D",
+      },
     },
   },
   plugins: [],
-}
+};
